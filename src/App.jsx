25d199d0ae840808d325d96e985a1f934da3cfa7
arthurgranito <arthurcursotecnico@gmail.com>
2025-06-18 15:47:34 -0300

    adição do useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
 import axios from "axios";
